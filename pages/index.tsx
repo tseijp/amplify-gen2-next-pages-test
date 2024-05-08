9818d51e985cf6ce2de8e49ccbdc9b90c3940685
tseijp <_@tsei.jp>
2024-05-08 18:59:15 +0900

    feat: added per-user data isolation

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,5 @@
+import '@aws-amplify/ui-react/styles.css'
+import { Authenticator } from '@aws-amplify/ui-react'
 import { useState, useEffect } from "react";
 import { generateClient } from "aws-amplify/data";
 import type { Schema } from "@/amplify/data/resource";
@@ -24,21 +26,26 @@ export default function App() {
   }
 
   return (
-    <main>
-      <h1>My todos</h1>
-      <button onClick={createTodo}>+ new</button>
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
-        ))}
-      </ul>
-      <div>
-        🥳 App successfully hosted. Try creating a new todo.
-        <br />
-        <a href="https://docs.amplify.aws/gen2/start/quickstart/nextjs-pages-router/">
-          Review next steps of this tutorial.
-        </a>
-      </div>
-    </main>
+    <Authenticator>
+      {({ signOut, user }) => (
+        <main>
+          <h1>{user?.signInDetails?.loginId + "'s todos"}</h1>
+          <button onClick={createTodo}>+ new</button>
+          <ul>
+            {todos.map((todo) => (
+              <li key={todo.id}>{todo.content}</li>
+            ))}
+          </ul>
+          <div>
+            🥳 App successfully hosted. Try creating a new todo.
+            <br />
+            <a href="https://docs.amplify.aws/gen2/start/quickstart/nextjs-pages-router/">
+              Review next steps of this tutorial.
+            </a>
+          </div>
+                    <button onClick={signOut}>Sign out</button>
+        </main>
+      )}
+    </Authenticator>
   );
 }
